33c64118bc77387edd8df758a66a88851a39b73d
Jonathan Gatz <J.Gatz712@gmail.com>
2022-11-12 23:14:09 -0500

    adjusted components

--- gatsby_portfolio/src/components/Layout/index.js
+++ gatsby_portfolio/src/components/Layout/index.js
@@ -1,8 +1,8 @@
-import './index.scss';
-
+import Sidebar from '../Sidebar'
+import './index.scss'
 
 const Layout = () => {
-    return <>Hello World</>
+  return <Sidebar />
 }
 
-export default Layout 
+export default Layout
