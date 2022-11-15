797f5911868453e84c81234d536afdb6efebce29
Jonathan Gatz <J.Gatz712@gmail.com>
2022-11-15 17:50:06 -0500

    initial sidebar build

--- gatsby_portfolio/src/components/Layout/index.js
+++ gatsby_portfolio/src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import Sidebar from '../Sidebar'
+//import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
--- gatsby_portfolio/src/components/Sidebar/index.js
+++ gatsby_portfolio/src/components/Sidebar/index.js
@@ -1,10 +1,17 @@
-// import { Link, NavLink } from 'react-router-dom'
-// import './index.scss'
-// import LogoS from '../../assets/images/logo-s.png'
-// import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
-// const Sidebar = () => <h1> I will be the infamous sidebar function </h1>
+const Sidebar = () =>  (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo' />
+            <img src={LogoSubtitle} alt='Jonathan' />
+        </Link>
+    </div>
+)
 
-// export default Sidebar
+export default Sidebar
