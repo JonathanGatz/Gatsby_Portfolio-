993ea3241a52df5e2acf48dc80fa4004840229a0
Jonathan Gatz <J.Gatz712@gmail.com>
2022-11-16 13:38:07 -0500

    further sidebar component build and style

--- gatsby_portfolio/src/components/Layout/index.js
+++ gatsby_portfolio/src/components/Layout/index.js
@@ -1,8 +1,12 @@
-//import Sidebar from '../Sidebar'
+import Sidebar from '../Sidebar'
 import './index.scss'
 
 const Layout = () => {
-  return <h1>➡️ Sidebar funtion will go here ⬅️</h1>
+  return (
+    <>
+      <Sidebar />
+    </>
+  )
 }
 
 export default Layout
--- gatsby_portfolio/src/components/Sidebar/index.js
+++ gatsby_portfolio/src/components/Sidebar/index.js
@@ -1,17 +1,99 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+//import Logo_Sub from '../../assets/images/logo_sub.png'
 
-const Sidebar = () =>  (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={LogoS} alt='logo' />
-            <img src={LogoSubtitle} alt='Jonathan' />
-        </Link>
-    </div>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faLinkedin,
+  faGithub,
+  faTwitter,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link>
+      <img className="logo" src={LogoS} alt="logo" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/jonathan-gatz?original_referer=https%3A%2F%2Fwww.google.com%2F"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/JonathanGatz"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/jonathan-gatz?original_referer=https%3A%2F%2Fwww.google.com%2F"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faTwitter}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.instagram.com/jonathangatz/?next=%2Fjonathangatzfitness%2F"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faInstagram}
+            color="#4d4d4e"
+            className="anchor-icon"
+          />
+        </a>
+      </li>
+    </ul>
+  </div>
 )
 
 export default Sidebar
